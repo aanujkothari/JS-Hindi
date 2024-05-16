5aa80980669702eb030d62448f4bbf02ca67f5fb
aanujkothari <96535694+aanujkothari@users.noreply.github.com>
2024-05-16 10:51:03 +0000

    commiting  variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+let productName = 'Microwave';
+let productType = 'Electronics';
+let productPrice = 35000;
+
+console.log(productName);
+console.log(productPrice);
+console.log(productType);
+
+console.table(productName,productPrice,productType);
